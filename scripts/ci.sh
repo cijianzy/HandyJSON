c76daadd528bbdc52a815951993e1575ea4fc1dd
此间 <zhangyue.zhangyue@alibaba-inc.com>
2016-09-25 01:35:43 +0800

    travis ci test

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,8 +2,5 @@
 
 set -e
 
-xcodebuild -workspace SwiftyJSON.xcworkspace -scheme "SwiftyJSON iOS" -destination "platform=iOS Simulator,name=iPhone 6" test
+xcodebuild -workspace HandyJSON.xcworkspace -scheme "HandyJSON iOS" -destination "platform=iOS Simulator,name=iPhone 6" test
 
-xcodebuild -workspace SwiftyJSON.xcworkspace -scheme "SwiftyJSON OSX" test
-
-xcodebuild -workspace SwiftyJSON.xcworkspace -scheme "SwiftyJSON tvOS" -destination "platform=tvOS Simulator,name=Apple TV 1080p" test
