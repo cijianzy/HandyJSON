29d43d68fcfda40013680fb483d354e4f7fc4cdb
此间 <zhangyue.zhangyue@alibaba-inc.com>
2016-10-04 08:57:35 +0800

    No macOS Test

--- script/ci.sh
+++ script/ci.sh
@@ -4,6 +4,6 @@ set -e
 
 xcodebuild -scheme "HandyJSON iOS Tests" -destination "platform=iOS Simulator,name=iPhone 6" test
 
-xcodebuild -scheme "HandyJSON macOS" test
+#xcodebuild -scheme "HandyJSON macOS" test
 
 xcodebuild -scheme "HandyJSON tvOS" -destination "platform=tvOS Simulator,name=Apple TV 1080p" test
